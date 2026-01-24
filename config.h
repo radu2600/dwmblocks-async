1dfcaedb25d9e3332ceea77b384374f590175b3f
Radu <rdmc10@github.com>
2026-01-24 18:32:23 +0200

    Personal config

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER "  "
+#define DELIMITER " | "
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 45
@@ -18,15 +18,12 @@
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)             \
-    X("", "sb-mail", 600, 1)  \
-    X("", "sb-music", 0, 2)   \
-    X("", "sb-disk", 1800, 3) \
-    X("", "sb-memory", 10, 4) \
-    X("", "sb-loadavg", 5, 5) \
-    X("", "sb-mic", 0, 6)     \
-    X("", "sb-record", 0, 7)  \
-    X("", "sb-volume", 0, 8)  \
-    X("", "sb-battery", 5, 9) \
-    X("", "sb-date", 1, 10)
+    X("", "/opt/dwmblocks/cputemp", 1, 0) \
+    X("", "/opt/dwmblocks/cpu", 1, 0) \
+    X("", "/opt/dwmblocks/updates", 60, 1) \
+    X("", "/opt/dwmblocks/memory", 2, 2) \
+    X("", "/opt/dwmblocks/uptime", 60, 3) \
+    X("", "/opt/dwmblocks/volume", 0, 4) \
+    X("", "/opt/dwmblocks/clock", 1, 5)
 
 #endif  // CONFIG_H
